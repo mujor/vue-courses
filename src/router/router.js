2a96d9c0fa284ee4b87180f05661ce6766f7616c
PeterParker <13280814369@163.com>
2018-09-11 09:25:23 +0800

    嵌套路由

--- src/router/router.js
+++ src/router/router.js
@@ -12,5 +12,11 @@ export default [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '@/views/About.vue')
+  },
+  
+  {
+    path: '/parent/:a',
+    name: 'parent',
+    component: () => import('@/views/parent.vue')
   }
 ]
---
+++ src/views/parent.vue
@@ -0,0 +1,5 @@
+<template>
+  <div>
+    I'm parent!{{a}}
+  </div>
+</template>
