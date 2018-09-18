785a43eadf44d4fb2e916075c8aeb6e06f8eebe3
PeterParker <13280814369@163.com>
2018-09-18 18:41:23 +0800

    1

---
+++ src/views/store.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <a-input v-model="inputValue"/>
+    <p> {{ inputValue }} </p>
+  </div>
+</template>
+
+<script>
+import AInput from '_c/AInput.vue'
+export default {
+  name: 'store',
+  data () {
+    return{
+      inputValue: 'aaa'
+    }
+  },
+  components:{
+    AInput
+  }
+}
+</script>
